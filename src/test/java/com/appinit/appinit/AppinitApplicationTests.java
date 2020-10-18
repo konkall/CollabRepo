50ee242637f52c0c294e32e792e1aaaa4ff4d646
Kostasdit <Katakalidis.Konstantinoseu@gmail.com>
2020-10-18 14:59:57 +0300

    Initial commit

---
+++ src/test/java/com/appinit/appinit/AppinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appinit.appinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppinitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
