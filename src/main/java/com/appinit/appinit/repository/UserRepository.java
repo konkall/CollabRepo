818249ed078607e468b6262da9e8ce8f4a8da691
Kostasdit <Katakalidis.Konstantinoseu@gmail.com>
2020-10-20 15:31:53 +0300

    Add firstname lastname to database

---
+++ src/main/java/com/appinit/appinit/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.appinit.appinit.repository;
+
+import com.appinit.appinit.model.DataStore;
+import org.springframework.data.repository.CrudRepository;
+
+//Spring Data = project where you can connect your program with databases
+//interface is an absctract class..cannot have a body because you cannot create objects
+//crud stands for Create Read Update Delete
+// 1 argument = Entity
+// 2 argumnet = type tou id
+public interface  UserRepository extends CrudRepository<DataStore, String> {
+}
