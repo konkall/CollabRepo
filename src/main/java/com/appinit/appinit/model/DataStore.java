94ee19d52d47c9c6081ef2e98a4f0da1861e29bf
konkall <72971081+konkall@users.noreply.github.com>
2020-10-24 19:36:33 +0300

    Added the Upload csv/insert to db functionality successfully.

--- src/main/java/com/appinit/appinit/model/DataStore.java
+++ src/main/java/com/appinit/appinit/model/DataStore.java
@@ -1,8 +1,6 @@
 package com.appinit.appinit.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -23,7 +21,9 @@ public class DataStore {
     @Size(min=8)
     private String password;
 
+    @Transient
     private String repassword;
+
 
     public void setEmail(String em){
         this.email = em;
