eb509d49fd6dc47546c8e93f05c637ec51d3f0c0
konkall <72971081+konkall@users.noreply.github.com>
2020-10-26 15:22:59 +0200

    Added early stages of "Search" functionality successfully. (Possibly not the optimal way to do it.)

--- src/main/java/com/appinit/appinit/repository/ClientRepository.java
+++ src/main/java/com/appinit/appinit/repository/ClientRepository.java
@@ -4,4 +4,14 @@ import com.appinit.appinit.model.Client;
 import org.springframework.data.repository.CrudRepository;
 
 public interface  ClientRepository extends CrudRepository<Client, Long> {
+    Iterable<Client> findAllByNameContaining(String name);
+    Iterable<Client> findAllBySsn(String ssn);
+    Iterable<Client> findAllByAccountType(String type);
+
+    Iterable<Client> findAllByNameAndSsn(String name, String ssn);
+    Iterable<Client> findAllByNameAndAccountType(String name, String type);
+    Iterable<Client> findAllBySsnAndAccountType(String ssn, String type);
+
+    Iterable<Client> findAllByNameAndSsnAndAccountType(String name, String ssn, String type);
+
 }
